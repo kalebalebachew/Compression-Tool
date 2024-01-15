10cec847ee1b1b3b68f5c2b44e1d0c2eefc83e02
Kaleb <Kalebalebachew4@gmail.com>
2024-01-15 23:06:19 +0300

    added simple svelte frontend

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import FileUploader from '/fileUploader.svelte';
+  </script>
+  
+  <main>
+	<h1> Compression tool</h1>
+	<FileUploader />
+  </main>
+  
+  <style>
+	main {
+	  text-align: center;
+	  margin: 20px;
+	}
+  </style>
+  
